18229c44d0912b4726630c0e11bbfbce656094ee
Siem26 <siemfesahazion2608@gmail.com>
2026-05-22 11:54:52 -0600

    Update base_enemy.gd
    Created _ready() and _take_damage methods

--- entities/enemies/base_enemy.gd
+++ entities/enemies/base_enemy.gd
@@ -1,1 +1,22 @@
 extends Area2D
+# 
+@export var max_health := 100.0
+@export var health = max_health
+
+@onready var health_bar := $UI/HealthBar
+
+func _ready() -> void:
+	health = max_health
+	health_bar.max_value = max_health
+	health_bar.value = health
+
+func _take_damage(amount: float) -> void:
+	if amount >= health:
+		health = 0.0
+		health_bar.value = health
+		queue_free()
+	else:
+		health -= amount
+		health_bar.value = health
+	
+	
