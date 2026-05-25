0b9e1b7ad36d4eaddf9c5683f83e15733d85bc28
Siem26 <siemfesahazion2608@gmail.com>
2026-05-25 11:04:11 -0600

    update base_enemy.gd
    Added player finding movement

--- entities/enemies/base_enemy.gd
+++ entities/enemies/base_enemy.gd
@@ -5,10 +5,29 @@ extends Area2D
 
 @onready var health_bar := $UI/HealthBar
 
+var player = null
+var direction := Vector2(0, 0)
+var enemy_speed := 50.0
+
+
 func _ready() -> void:
 	health = max_health
 	health_bar.max_value = max_health
 	health_bar.value = health
+	player = get_tree().get_nodes_in_group("Player")
+
+func _proccess(delta: float) -> void:
+	if player == null:
+		return
+	
+	look_at(player.global_position)
+	direction = (player.global_position - global_position).normalized()
+	var velocity = (direction * enemy_speed)
+	global_position += (velocity * delta)
+
+
+
+
 
 func _take_damage(amount: float) -> void:
 	if amount >= health:
