c6674130d2fcc5b1ae505c2174e71328ab27f2f2
Siem26 <siemfesahazion2608@gmail.com>
2026-05-25 11:17:16 -0600

    Update base_enemy.tscn
    Added timer Node

--- entities/enemies/base_enemy.gd
+++ entities/enemies/base_enemy.gd
@@ -2,31 +2,38 @@ extends Area2D
 # 
 @export var max_health := 100.0
 @export var health = max_health
-
 @onready var health_bar := $UI/HealthBar
+@export var projectile_scene: PackedScene
+@onready var timer = $Timer
 
-var player = null
+
+var player = null # player node
 var direction := Vector2(0, 0)
-var enemy_speed := 50.0
+var enemy_speed := 50.0  # enemy speed
 
 
 func _ready() -> void:
 	health = max_health
 	health_bar.max_value = max_health
 	health_bar.value = health
-	player = get_tree().get_nodes_in_group("Player")
+	player = get_tree().current_scene.get_nodes_in_group("Player")
+	timer.wait_time = 1.0
+	timer.start()
 
-func _proccess(delta: float) -> void:
+func _physics_proccess(delta: float) -> void:
 	if player == null:
 		return
-	
+		
+	# Finds player and moves towards player
 	look_at(player.global_position)
 	direction = (player.global_position - global_position).normalized()
 	var velocity = (direction * enemy_speed)
-	global_position += (velocity * delta)
+	global_position += (transform.x * velocity * delta)
 
 
-
+func shoot_at_player() -> void:
+	
+	pass
 
 
 func _take_damage(amount: float) -> void:
@@ -37,3 +44,7 @@ func _take_damage(amount: float) -> void:
 	else:
 		health -= amount
 		health_bar.value = health
+
+
+func _on_timer_timeout() -> void:
+	shoot_at_player()
