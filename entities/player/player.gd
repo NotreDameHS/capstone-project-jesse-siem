ce24a37ca271b49a6baa0bec347e87c4e1947cff
Siem26 <siemfesahazion2608@gmail.com>
2026-05-27 14:45:20 -0600

    Update player.gd

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -15,11 +15,18 @@ func _ready() -> void:
 	
 	
 func _process(delta: float) -> void:
-	
+	# Rotates ship 90 degrees to the right to make RIGHT = right
 	rotation += deg_to_rad(90)
+	
+	# Rotates ship to point towards mouse_global_position
 	var  mouse_global_pos = get_global_mouse_position()
 	look_at(mouse_global_pos)
 	
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+	
+func _physics_process(delta: float) -> void:
+	# Movement for ship
 	var direction := Vector2(0, 0)
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
@@ -32,7 +39,6 @@ func _process(delta: float) -> void:
 	var steering_vector := (desired_velocity - velocity)
 	velocity += (steering_vector * steering_factor * delta)
 	global_position += (velocity * delta)
-	
 	
 	
 func shoot() -> void:
