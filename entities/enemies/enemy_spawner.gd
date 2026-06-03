3a6f57ba78a4869aa6589ac65e48757880753834
Siem26 <siemfesahazion2608@gmail.com>
2026-06-03 14:35:05 -0600

    update enemy_spawner.gd

--- entities/enemies/enemy_spawner.gd
+++ entities/enemies/enemy_spawner.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+@export var round_1_enemies: PackedScene
+@export var round_2_enemies: PackedScene
+@export var final_boss: PackedScene
+var max_enemy_spawns := 5
+var time_between_spawn := 2.0
+
+
+func _ready() -> void:
+	pass
