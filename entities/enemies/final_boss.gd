41eecd50be845be4111f0f47636989c5df449265
Siem26 <siemfesahazion2608@gmail.com>
2026-06-09 09:24:59 -0600

    update final_boss.gd

--- entities/enemies/final_boss.gd
+++ entities/enemies/final_boss.gd
@@ -6,6 +6,9 @@ class_name Boss extends Enemy
 @onready var boss_marker_2 = $Marker2D2
 @onready var boss_marker_3 = $Marker2D3
 @onready var boss_marker_4 = $Marker2D4
+@export var boss_projectile_scene: PackedScene
+
+
 
 #func _physics_process(delta: float) -> void:
 	#if player == null:
@@ -40,13 +43,13 @@ func shoot_at_player() -> void:
 	var direction_2 = (player.global_position - global_position).normalized()
 	projectile_2.direction = direction_2
 	
-	var projectile_3 = projectile_scene.instantiate()
+	var projectile_3 = boss_projectile_scene.instantiate()
 	get_tree().current_scene.add_child(projectile_3)
 	projectile_3.global_position = boss_marker_3.global_position
 	var direction_3 = (player.global_position - global_position).normalized()
 	projectile_3.direction = direction_3
 	
-	var projectile_4 = projectile_scene.instantiate()
+	var projectile_4 = boss_projectile_scene.instantiate()
 	get_tree().current_scene.add_child(projectile_4)
 	projectile_4.global_position = boss_marker_4.global_position
 	var direction_4 = (player.global_position - global_position).normalized()
