0a6ca5ff3c39fff264d6fe18174de3eef74ecd95
Siem26 <siemfesahazion2608@gmail.com>
2026-06-10 08:30:57 -0600

    update projectile scripts

--- entities/enemies/base_enemy.gd
+++ entities/enemies/base_enemy.gd
@@ -79,7 +79,7 @@ func _take_damage(amount: float) -> void:
 		get_tree().current_scene.add_child(damage_indicator)
 		damage_indicator.global_position = global_position
 		damage_indicator.display_damage(amount)
-		spawn_poof()
+		#spawn_poof()
 
 
 
@@ -90,42 +90,5 @@ func _on_timer_timeout() -> void:
 	
 
 
-func spawn_poof():
-	# Creating newCPUParticles2D node
-	var particles = CPUParticles2D.new()
-	get_tree().current_scene.add_child(particles)
-	particles.global_position = global_position
-	
-	
-# Create a particle cloud (a "poof" of particles from the center)
-	particles.z_index = 100 
-	particles.z_as_relative = false 
-	particles.amount = 20
-	particles.lifetime = 0.5
-	particles.explosiveness = 1.0
-	particles.one_shot = true
-	particles.scale_amount_min = 10.0 
-	particles.scale_amount_max = 20.0
-	particles.spread = 180.0
-	particles.gravity = Vector2(0, 0)
-	particles.initial_velocity_min = 80.0
-	particles.initial_velocity_max = 150.0
-	particles.damping_min = 50.0 
 
-	# Design the shape of the cloud (the "poof")
-	var curve = Curve.new()
-	curve.add_point(Vector2(0, 1.0)) 
-	curve.add_point(Vector2(1, 0.0))
-	particles.scale_amount_curve = curve
-
-	# Design the colours of the cloud
-	var gradient = Gradient.new()
-	gradient.add_point(0.0, Color(1, 1, 1, 1)) 
-	gradient.add_point(1.0, Color(1, 1, 1, 0)) 
-	particles.color_ramp = gradient
-
-	particles.emitting = true
-	
-	var timer = get_tree().create_timer(particles.lifetime + 0.5)
-	timer.timeout.connect(particles.queue_free)
 	
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -100,7 +100,7 @@ func _take_damage(amount: float) -> void:
 		player_health -= amount
 		health_bar.value = player_health
 		print(player_health)
-		spawn_poof()
+		
 	
 	
 func kill_amount(kill_count: int) -> void:
@@ -141,42 +141,5 @@ func _on_invincibility_timer_timeout() -> void:
 	print("Invincibility powerup is over!")
 	
 
-func spawn_poof():
-	# Creating newCPUParticles2D node
-	var particles = CPUParticles2D.new()
-	get_tree().current_scene.add_child(particles)
-	particles.global_position = global_position
-	
-	
-# Create a particle cloud (a "poof" of particles from the center)
-	particles.z_index = 100 
-	particles.z_as_relative = false 
-	particles.amount = 20
-	particles.lifetime = 0.5
-	particles.explosiveness = 1.0
-	particles.one_shot = true
-	particles.scale_amount_min = 10.0 
-	particles.scale_amount_max = 20.0
-	particles.spread = 180.0
-	particles.gravity = Vector2(0, 0)
-	particles.initial_velocity_min = 80.0
-	particles.initial_velocity_max = 150.0
-	particles.damping_min = 50.0 
 
-	# Design the shape of the cloud (the "poof")
-	var curve = Curve.new()
-	curve.add_point(Vector2(0, 1.0)) 
-	curve.add_point(Vector2(1, 0.0))
-	particles.scale_amount_curve = curve
-
-	# Design the colours of the cloud
-	var gradient = Gradient.new()
-	gradient.add_point(0.0, Color(1, 1, 1, 1)) 
-	gradient.add_point(1.0, Color(1, 1, 1, 0)) 
-	particles.color_ramp = gradient
-
-	particles.emitting = true
-	
-	var timer = get_tree().create_timer(particles.lifetime + 0.5)
-	timer.timeout.connect(particles.queue_free)
 	
